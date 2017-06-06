10833eab1fe0f6495edc4f17a5d2996cde9f7589
Egon Willighagen <egon.willighagen@gmail.com>
2017-06-06 06:49:07 +0200

    Added a template for the specification; needs new branding

---
+++ docs/bibref/biblio.js
@@ -0,0 +1,3 @@
+berjon.biblio = {
+   "Marshall2012" : "Marshall, M. S.;  Boyce, R.;  Deus, H. F.;  Zhao, J.;   Willighagen, E. L.;  Samwald, M.;  Pichler, E.;  Hajagos, J.;   Prud'hommeaux, E.;  Stephens, S. Emerging practices for mapping and linking life sciences data using RDF - A case series, <a href=\"http://dx.doi.org/10.1016/j.websem.2012.02.003\">http://dx.doi.org/10.1016/j.websem.2012.02.003</a>."
+}
